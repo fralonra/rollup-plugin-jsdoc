be622d7d4fb885b8c87508b8b83b965c7cc7c9d8
fralonra <fralonra@aliyun.com>
2020-01-04 21:55:25 +0800

    fix indents

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import pkg from './package.json'
 
 export default {
-	input: 'src/index.js',
-	plugins: [],
-	external: Object.keys(pkg.dependencies).concat(['child_process', 'path']),
-	output: [{
+  input: 'src/index.js',
+  plugins: [],
+  external: Object.keys(pkg.dependencies).concat(['child_process', 'path']),
+  output: [{
     format: 'es',
     file: pkg.module,
     sourcemap: true
